9ef7ed46d5f8e34073c7971a843a76d0bb717df4
vjaviertaype <vjaviertaype@outlook.com>
2025-08-02 11:43:02 -0300

    add: eliminando importacion innexistente

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   controllers: [AppController],
   providers: [PrismaService],
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule],
 })
 export class AppModule {}
